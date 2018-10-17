8b81b15ca2c4f9a023b544c658a0e2da3ff46630
iamjunior <myjuniorprog@gmail.com>
2018-10-17 08:18:24 +0700

    Tutorial 12

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = [
+            ['id' => '1', 'title' => 'Post Title 1', 'body' => 'Post Body 1'],
+            ['id' => '2', 'title' => 'Post Title 2', 'body' => 'Post Body 2'],
+            ['id' => '3', 'title' => 'Post Title 3', 'body' => 'Post Body 3'],
+            ['id' => '4', 'title' => 'Post Title 4', 'body' => 'Post Body 4'],
+            ['id' => '5', 'title' => 'Post Title 5', 'body' => 'Post Body 5'],
+        ];
+
+        return view('index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return dd($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return 'ini halaman show dengan id:'. $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return 'Halaman edit dengan id data:'. $id;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function(){
+    return "Hi, This about Page";
+});
+
+Route::get('/blog', 'PostController@index');
+// Route::get('/post/create', 'PostController@create');
+// Route::post('/post/store', 'PostController@store');
+
+// Route::get('/post/{id}',['as' => 'post.detail',function($id){
+//     echo "Post $id";
+//     echo "<br/>";
+//     echo "Body Post In ID $id";
+// }]);
+
+Route::resource('post','PostController');
